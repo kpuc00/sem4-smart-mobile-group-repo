9b84234450829ac74f4e635088410a76e1c63573
krishum77 <balccbf@gmail.com>
2021-03-16 16:28:20 +0100

    fixed service-worker

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -2,19 +2,15 @@ const cacheName = "KPUC_V1";
 const appFiles = [
   "/manifest.json",
   "/js/scripts.js",
-  "/css/style.css",
-  "/images/background.jpg",
-  "/images/favicon.ico",
-  "/images/logo-nav.png",
-  "/images/logo.png"
+  "/css/style.css"
 ];
 
 self.addEventListener("install", (installing) => {
   installing.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log("Service Worker: Caching important offline files");
-      return cache.addAll(appFiles);
-    })
+      caches.open(cacheName).then((cache) => {
+        console.log("Service Worker: Caching important offline files");
+        return cache.addAll(appFiles);
+      })
   );
 });
 
@@ -24,39 +20,39 @@ self.addEventListener("activate", (activating) => {
 
 self.addEventListener("fetch", (fetching) => {
   fetching.respondWith(
-    caches.match(fetching.request.url).then((response) => {
-      console.log("Service Worker: Fetching resource " + fetching.request.url);
-      return response || fetch(fetching.request).then((response) => {
-        console.log("Service Worker: Resource " + fetching.request.url + " not available in cache");
-        return caches.open(cacheName).then((cache) => {
-          console.log("Service Worker: Caching (new) resource " + fetching.request.url);
-          cache.put(fetching.request, response.clone());
-          return response;
-        });
-      }).catch(function () {
-        console.log("Service Worker: Fetching online failed, HAALLPPPP!!!");
-        //Do something else with the request (respond with a different cached file)
+      caches.match(fetching.request.url).then((response) => {
+        console.log("Service Worker: Fetching resource " + fetching.request.url);
+        return response || fetch(fetching.request).then((response1) => {
+          console.log("Service Worker: Resource " + fetching.request.url + " not available in cache");
+          return caches.open(cacheName).then((cache) => {
+            console.log("Service Worker: Caching (new) resource " + fetching.request.url);
+            cache.put(fetching.request, response1.clone());
+            return response1;
+          });
+        }).catch(function () {
+          console.log("Service Worker: Fetching online failed, HAALLPPPP!!!");
+          //Do something else with the request (respond with a different cached file)
+        })
       })
-    })
   );
 });
 
 self.addEventListener("push", (pushing) => {
   if(pushing.data){
-    pushdata=JSON.parse(pushing.data.text());		
+     let pushdata=JSON.parse(pushing.data.text());
     console.log("Service Worker: I received this:",pushdata);
-    if((pushdata["title"]!="")&&(pushdata["message"]!="")){			
+    if((pushdata["title"]!=="")&&(pushdata["message"]!=="")){
       const options={ body:pushdata["message"] }
       self.registration.showNotification(pushdata["title"],options);
       console.log("Service Worker: I made a notification for the user");
     } else {
-      console.log("Service Worker: I didn't make a notification for the user, not all the info was there :(");			
+      console.log("Service Worker: I didn't make a notification for the user, not all the info was there :(");
     }
   }
 });
 
 self.addEventListener("notificationclick",function(clicking){
   const pageToOpen="/";
-	const promiseChain=clients.openWindow(pageToOpen);
-	event.waitUntil(promiseChain);
+  const promiseChain=clients.openWindow(pageToOpen);
+  event.waitUntil(promiseChain);
 });
