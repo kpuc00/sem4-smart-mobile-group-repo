739c69b140a310bfea131a030ce936a08c5dc885
krishum77 <balccbf@gmail.com>
2021-03-18 11:21:24 +0100

    filters

--- pwa/js/getAllJobs.js
+++ pwa/js/getAllJobs.js
@@ -7,7 +7,7 @@ $.ajax({
     success: function (data){
         $.each(data, function(index, element) {// purvite 3x31 i 3 tochki ako e nad 3x34
             const card =  '<div class="my-3">\n' +
-                '                            <div class="card" style="width: 18rem;">\n' +
+                '                            <div class="card" style="width: 18rem; margin: auto;">\n' +
                 '                                <div class="card-body">\n' +
                 '                                    <h5 class="card-title">' + element.company + '</h5>\n' +
                 '                                    <p class="card-text">'
