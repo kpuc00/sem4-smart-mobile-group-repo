94e6ee56bd679e41f9f64b59583ed8e5139d1bd9
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2021-04-12 02:33:18 +0200

    added list of hobbies in profile page activity

---
+++ android/app/src/main/java/com/hills_studio/HobbiesListAdapter.kt
@@ -0,0 +1,40 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.hills_studio.R
+
+internal class HobbiesListAdapter internal constructor(
+    context: Context,
+    private val resource: Int,
+    private val hobbiesList: Array<String>?
+) : ArrayAdapter<HobbiesListAdapter.ItemHolder>(context, resource) {
+
+    override fun getCount(): Int {
+        return if (this.hobbiesList != null) this.hobbiesList.size else 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var convertView = convertView
+
+        val holder: ItemHolder
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(resource, null)
+            holder = ItemHolder()
+            holder.hobbyTitle = convertView!!.findViewById(R.id.hobbyTitle)
+            convertView.tag = holder
+        } else {
+            holder = convertView.tag as ItemHolder
+        }
+
+        holder.hobbyTitle!!.text = this.hobbiesList!![position]
+
+        return convertView
+    }
+
+    internal class ItemHolder {
+        var hobbyTitle: TextView? = null
+    }
+}
--- android/app/src/main/java/com/hills_studio/ProfilePageActivity.kt
+++ android/app/src/main/java/com/hills_studio/ProfilePageActivity.kt
@@ -1,11 +1,26 @@
 package com.hills_studio
 
-import androidx.appcompat.app.AppCompatActivity
+import HobbiesListAdapter
 import android.os.Bundle
+import android.widget.GridView
+import androidx.appcompat.app.AppCompatActivity
 
 class ProfilePageActivity : AppCompatActivity() {
+    private val hobbiesList: Array<String>
+        get() = arrayOf(
+            "Football",
+            "Painting",
+            "Fishing",
+            "Hiking",
+            "Sailing"
+        )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_page)
+
+        val gvHobbies = findViewById<GridView>(R.id.gvHobbies)
+        val adapter = HobbiesListAdapter(this, R.layout.item_hobby, hobbiesList)
+        gvHobbies.adapter = adapter
     }
 }
