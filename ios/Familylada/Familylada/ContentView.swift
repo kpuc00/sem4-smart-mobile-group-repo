b0fb981b3b587834a2611d6c4cf6f22d6b12a167
krishum77 <balccbf@gmail.com>
2021-05-12 15:24:41 +0300

    navigation bars

--- ios/Familylada/Familylada/ContentView.swift
+++ ios/Familylada/Familylada/ContentView.swift
@@ -9,13 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+            TabView{
+                getnavigation()
+                    .tabItem{
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                    
+                    
+                getnavigation()
+                    .tabItem{
+                        Image(systemName: "rectangle.stack.person.crop")
+                        Text("Collections")
+                        
+                    }
+                getnavigation()
+                    .tabItem{
+                        Image(systemName: "clock")
+                        Text("Events")
+                        
+                    }
+                
+                getnavigation()
+                    .tabItem{
+                        Image(systemName: "person.3")
+                        Text("Members")
+                        
+                    }
+                getnavigation()
+                    .tabItem{
+                        Image(systemName: "questionmark.circle")
+                        Text("Family+")
+                        
+                    }
+            
+        }
     }
 }
 
+@ViewBuilder
+func getnavigation() -> some View {
+    NavigationView{
+        Text("FamilyLada")
+            .toolbar {
+                ToolbarItem(placement: .principal){
+                    Text("Familylada")
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        print("rightBtn")
+                    }) {
+                        Image(systemName: "gear")
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        print("leftBtn")
+                    }) {
+                        Image(systemName: "person.circle.fill")
+                    }
+                }
+            }
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
---
+++ ios/Familylada/Familylada/MyProfileView.swift
@@ -0,0 +1,10 @@
+import Foundation
+import SwiftUI
+
+struct MyProfileView: View{
+    var body: some View{
+        NavigationView{
+            let icon  = UIImage(systemName: "settings")
+    }
+}
+}
