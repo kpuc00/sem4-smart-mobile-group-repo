8953bd3b99e84ed1663dac034a64811b0b8b3f41
krishum77 <balccbf@gmail.com>
2021-05-13 11:20:50 +0300

    transferred event page to the right view

--- ios/Familylada/Familylada/EventsView.swift
+++ ios/Familylada/Familylada/EventsView.swift
@@ -8,10 +8,51 @@
 import SwiftUI
 
 struct EventsView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        ScrollView {
-            Text("Events view")
+        VStack{
+            Spacer()
+            HStack{
+                EventCardView(card: EventCard.example)
+                EventCardView(card: EventCard.example)
+            }
+            HStack{
+                EventCardView(card: EventCard.example)
+                EventCardView(card: EventCard.example)
+            }
+            HStack{
+                EventCardView(card: EventCard.example)
+                EventCardView(card: EventCard.example)
+            }
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        showingAlert = true
+                    }, label: {
+                        Text("+")
+                            .font(.system(.largeTitle))
+                            .frame(width: 77, height: 70)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 7)
+                    })
+                    .alert(isPresented: $showingAlert) {
+                                Alert(title: Text("Add new Event"), message: Text("hmmm"), dismissButton: .default(Text("Got it!")))
+                            }
+                    .background(Color.blue)
+                    .cornerRadius(38.5)
+                    .padding()
+                    .shadow(color: Color.black.opacity(0.3),
+                            radius: 3,
+                            x: 3,
+                            y: 3)
+                }
+            }
         }
+        
     }
 }
 
